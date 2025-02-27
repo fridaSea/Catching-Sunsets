d1772d3104589c16e2068df3b048a05618fe1b9e
fridaSea <maria.kluge.codes@gmail.com>
2025-02-27 16:07:54 +0100

    Set up the navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,23 @@
-import { BrowserRouter, Route, Routes } from "react-router";
-import Home from '../pages/Home/Home';
-import Registration from '../pages/Registration/Registration';
+import { BrowserRouter, Outlet, Route, Routes } from "react-router";
+import Home from './pages/Home/Home';
+import Registration from './pages/Registration/Registration';
 
 
 import './App.css'
+import Navbar from "./components/Navbar/Navbar";
+import Sunsets from "./pages/Sunsets/Sunsets";
+import Profile from "./pages/Profile/Profile";
 
-// const Root = () => {
-//   return (
-//     <>
-//     TO DO - Navbar/ Footer einfügen
-//     </>
-//   )
-// }
+const Root = () => {
+  return (
+    <>
+    {/* TO DO - Footer einfügen */}
+     <Navbar/>  
+     <Outlet/>  
+     {/* <Footer/>   */}
+    </>
+  )
+}
 
 
 function App() {
@@ -21,13 +27,15 @@ function App() {
     {/* <AuthContextProvider> */}
       <BrowserRouter>
         <Routes>
-          {/* <Route element={<Root/>}> */}
-          {/* <Route index element={<Home/>}/> */}
+          <Route element={<Root/>}>
+          <Route index element={<Home/>}/>
           <Route path="/home" element={<Home/>}/>
           <Route path="/registration" element={<Registration/>}/>
-          {/* <Route path="/sunsets" element={<Sunsets/>}/> */}
+          <Route path="/sunsets" element={<Sunsets/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+          {/* <Route path="/faq" element={<FAQ/>}/> */}
 
-          {/* </Route> */}
+          </Route>
         </Routes>
       </BrowserRouter>
      {/* </AuthContextProvider> */}
--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,10 +1,71 @@
-import React from 'react'
+import { useState } from 'react'
+import './Navbar.css'
+import { NavLink } from 'react-router';
+// import profile_icon from '../../../public/profile_icon.png'
 
+
+// Responsive Navbar with animated hamburger menu
 function Navbar() {
+    // const {user, login, logout} = useContext(AuthContext)
+
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const openMenu = () => {
+      setIsOpen(!isOpen);
+    }
+
+
   return (
-    <nav>
-      
-    </nav>
+    <header className='header'>
+        <div className='header-content'>
+            <div className='logo-container'>
+            <a href='/' className='logo'>Catching Sunsets</a>
+            </div>
+            
+            <div className='nav-container'>
+            <nav className={"nav " + (isOpen ? 'nav--open' : '')} >
+                <ul className='nav-list'>
+                    
+                    <li className='nav-item'>
+                        <NavLink 
+                        to='/sunsets' 
+                        className={({isActive}) => isActive ? 'nav-link nav-active' : 'nav-link'}
+                        onClick={() => setIsOpen(false)}
+                        >
+                            Sunsets
+                        </NavLink>
+                    </li>
+                    <li className='nav-item'>
+                        <NavLink 
+                        to='/registration' 
+                        className={({isActive}) => isActive ? 'nav-link nav-active' : 'nav-link'}
+                        onClick={() => setIsOpen(false)}>
+                            Registration
+                        </NavLink>
+                    </li>
+                    <li className='nav-item'>
+                        <NavLink 
+                        to='/profile' 
+                        className={({isActive}) => isActive ? 'nav-link nav-active' : 'nav-link'} 
+                        onClick={() => setIsOpen(false)}
+                        >
+                            Profile
+                            {/* <img src={profile_icon} alt="Profile-Icon" /> */}
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+            </div>
+
+        <div className={`hamburger-menu ${isOpen ? 'hamburger-menu--open' : ''}`}
+             onClick={openMenu} >
+            <div className='bar'></div>
+            <div className='bar'></div>
+            <div className='bar'></div>
+        </div>
+        </div>
+         
+         
+    </header>
   )
 }
 
