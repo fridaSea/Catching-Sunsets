6c76798ebccf11575978b2326f0e6fc237173bda
fridaSea <maria.kluge.codes@gmail.com>
2025-03-12 20:04:28 +0100

    Custom hook: useFetch()

---
+++ client/src/hooks/useFetch.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+type HookReturnType<T> = {
+  data: T | null;
+};
+
+function useFetch<T>(url: string): HookReturnType<T> {
+  const [data, setData] = useState<T | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(url);
+      const result = (await response.json()) as T;
+      //   console.log('result:>>', result)
+      setData(result);
+    };
+    fetchData();
+  }, [url]);
+
+  return {
+    data: data,
+  };
+}
+
+export default useFetch;
