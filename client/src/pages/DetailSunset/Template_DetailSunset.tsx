4f5681ec1fa731aa441f4d8f3308d56e19cb69b4
fridaSea <maria.kluge.codes@gmail.com>
2025-03-16 20:30:20 +0100

    Set template for showing/ get one item

---
+++ client/src/pages/DetailSunset/Template_DetailSunset.tsx
@@ -0,0 +1,38 @@
+// import { useContext, useEffect, useState } from "react";
+// import { useParams } from "react-router";
+// import { getSunsetApi } from "../../api/sunset";
+// import { NewSunset } from "../../types/customTypes";
+// import { MenuContext } from "../../context/MenuContext";
+
+// function DetailSunset() {
+//   const { isMenuOpen, setIsMenuOpen } = useContext(MenuContext);
+//   const { id } = useParams();
+//   const [sunset, setSunset] = useState<NewSunset | null>(null);
+
+//   const getSunset = async () => {
+//     getSunsetApi(id).then(
+//       (sunset) => {
+//         setSunset(sunset);
+//       },
+//       (error) => {
+//         console.log("error :>> ", error);
+//       }
+//     );
+//   };
+
+//   useEffect(() => {
+//     getSunset();
+//   });
+
+//   return (
+//     <div
+//       className={`profile component-content-container ${
+//         isMenuOpen ? "content-container-menu-open" : ""
+//       }`}
+//     >
+//       {sunset?.description}
+//     </div>
+//   );
+// }
+
+// export default DetailSunset;
