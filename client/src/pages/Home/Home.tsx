8b83c7bb579feb36d869b0b8f5b39eb49f4061bc
fridaSea <maria.kluge.codes@gmail.com>
2025-03-24 19:27:14 +0100

    Implementing home page with content

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,19 +1,37 @@
 import { useContext } from "react";
-import Hero from "../../src/assets/Hero.jpeg";
 import { MenuContext } from "../../context/MenuContext";
+import HeroImage from "../../assets/Hero.png";
+import "./Home.css";
 
 const Home = () => {
   const { isMenuOpen } = useContext(MenuContext);
   return (
     <>
-      {/* <img src={Hero} alt="Sunset Picture" /> */}
       <div
-        className={`component-content-container ${
+        className={`home component-content-container ${
           isMenuOpen ? "content-container-menu-open" : ""
         }`}
       >
-        <h1>Happy Welcome :) </h1>
-        <p> Here you can find a lot of interesting content.</p>
+        <div className="hero-container">
+          <div className="hero-text">
+            <h1>Happy Welcome to Catching Sunsets</h1>
+            <br />
+            <p>
+              Let us enjoy the magic of sunsets together.
+              <br />
+              It's like the world is holding its breath for a moment, just to
+              show us how beautiful it can be.
+            </p>
+            <br />
+            <img className="hero-image" src={HeroImage} alt="Sunset" />
+            <br />
+            <p>
+              Let us catch some sunsets
+              <br />
+              😊
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
