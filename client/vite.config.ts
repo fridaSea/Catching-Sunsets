0655a646981acd78080b40581d6a80bd9704bf91
fridaSea <maria.kluge.codes@gmail.com>
2025-03-26 19:42:21 +0100

    Add manifest for PWA settings

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,41 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
+
+const manifestForPlugIn: Partial<VitePWAOptions> = {
+  registerType: "autoUpdate",
+  manifest: {
+    name: "Catching Sunsets",
+    short_name: "Catching Sunsets",
+    description:
+      "This app was created to share our favorite sunsets and to enjoy the magic of sunsets together.",
+    icons: [
+      {
+        src: "assets/sunset_pwa_icon_x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+      {
+        src: "assets/sunset_pwa_icon_x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+    theme_color: "#171717",
+    background_color: "#000000",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugIn)],
   //To make sure vite starts always in the same port, to match CORS options
   server: {
     port: 5173,
